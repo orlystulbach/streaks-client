b1b66d8a6819e5cde6c45b97fb9d075d840beec5
orlystulbach <o.stulbach@wustl.edu>
2024-11-18 11:25:15 -0600

    learning with jayce's instructions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+
+function AddHabit() {
+
+  const [count, setCount] = useState(1);
+
+  function handleClick() {
+    alert('You want to add a habit!');
+    setCount(count+1);
+  }
+  
+  return (
+    // Pass down handler function
+    <button onClick={handleClick}> 
+      Add Habit #{count}
+    </button>
+  );
+}
+
+function Greeting({ name }) {
+  return <h1>Hello, {name}</h1>
+}
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+  // const habits = [
+  //   { title: 'Reading', id: 1 },
+  //   { title: 'Learning', id: 2 },
+  //   { title: 'FaceTiming Parents', id: 3 },
+  // ];
+
+  // const listItems = habits.map(habit =>
+  //   <li key={habit.id}>
+  //     {habit.title}
+  //   </li>
+  // );
+
+  // return (
+  //   // Everything must be wrapped in a single shared parent
+  //   <>
+  //     <img src="landscape.jpg" className="landscape" />
+  //     <Greeting name="Orly"/>
+  //     <h4>Keep Track of Your Habits</h4>
+  //     <AddHabit />
+  //     <ul>{listItems}</ul>
+  //   </>
+  // )
 }
 
+// Specify main component of the file
 export default App
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,38 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import Root from "./routes/root"
+import ErrorPage from "./error-page"
+import Contact from "./routes/contact"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+  // {
+  //   path: "contacts/:contactId",
+  //   element: <Contact />,
+  // },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <RouterProvider router={router} />
     <App />
   </StrictMode>,
 )
